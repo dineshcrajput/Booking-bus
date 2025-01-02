1b562287dc5f7ddf5f7204a7e700352085044e07
dinesh <dinesh.rajput@advantawitty.com>
2025-01-02 15:39:19 +0530

    working on searching page

--- booking-bus/src/app/app.routes.ts
+++ booking-bus/src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
+import { SearchComponent } from './pages/search/search.component';
+import { BookingComponent } from './pages/booking/booking.component';
 
 export const routes: Routes = [
 
@@ -6,5 +8,15 @@ export const routes: Routes = [
         path:'',
         redirectTo:'search',
         pathMatch:'full'
+    },
+    {
+        path:'search',
+        component:SearchComponent
+
+    },
+    {
+        path:'booking',
+        component:BookingComponent
+
     }
 ];
--- booking-bus/src/app/pages/search/search.component.ts
+++ booking-bus/src/app/pages/search/search.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MasterService } from '../../service/master.service';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  masterServ = inject(MasterService);
+  locations$: Observable<any[]> = new Observable<any[]>;
 
+  ngOnInit(): void {
+    this.getAllLocations();
+  }
+
+  getAllLocations() {
+    this.locations$ = this.masterServ.getLocations();
+  }
 }
